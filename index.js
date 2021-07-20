fde91c407997b2ad89d6d84443d2f4f4035cfe82
Alex Kazakov <kazakov.a.s.90@gmail.com>
2021-07-20 22:44:57 +0100

    Add ncc

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const github = require('@actions/github');
 
 try {
   const name = core.getInput('name');
-  console.log(`Hello ${name}`);
+  console.log(`Hello ${name}!`);
   const time = (new Date()).toTimeString();
   core.setOutput('time', time);
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`event payload ${payload)`);
+  console.log(`event payload ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
